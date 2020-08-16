cfe9e01b7838b412a70f595aead8385d43a32b7b
Jeff Neuberger <jeffreyneuberger.com>
2020-08-16 16:04:36 -0400

    feat: led blinks on/off 5 times

--- led.py
+++ led.py
@@ -1,12 +1,13 @@
 from gpiozero import LED, Button
-from signal import pause
+from time import sleep
 
 led = LED(17)
-button = Button(16)
 count = 0
 
-while True:
+while count < 5:
     print(count)
-    button.when_pressed = led.on
-    button.when_released = led.off
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
     count += 1
